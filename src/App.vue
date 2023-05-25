064ad9140329d4d60e97ea6935b344f1779c0aea
Ken Terakado <tken915@gmail.com>
2023-05-25 10:53:23 +0900

    modified to accomodate multiple pages

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+        app
+        color="white"
+        dark
+        prominent
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Menu</v-toolbar-title>
+          <div class="d-flex align-center">
+            <v-img
+                alt="cook-logo"
+                class="shrink mr-2"
+                contain
+                src="assets/cook-logo.png"
+                transition="scale-transition"
+                width="40"
+            ></v-img>
+              </div>
+              <v-spacer></v-spacer>  
+    </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        bottom
+        temporary
+      >
+        <v-list
+            nav
+            dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item link>
+                  <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                  <v-list-item-title>Apple Pie</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                  <v-list-item-title>Chicken Alfredo</v-list-item-title>
+              </v-list-item>
+                  </v-list-item-group>
+              </v-list>
+          </v-navigation-drawer>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
